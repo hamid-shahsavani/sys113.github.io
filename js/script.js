200413e3d5e04bd7270f198a72c059ecd0ad0d3b
sys113 <051.SYS113@gmail.com>
2022-02-23 22:23:07 +0000

    remove tools number
    * remove tools number in main category

--- js/script.js
+++ js/script.js
@@ -236,41 +236,6 @@ $(".image-tool").click(function () {
   });
 });
 
-// ------------------------------------------------------------ number tools animation --------------------------------------------------------------------
-
-function constant (duration, range, current) {
-  return duration / range;
-}
-
-function animateValue(id, start, duration, easing) {
-  var end = parseInt(document.getElementById(id).textContent, 10);
-  var range = end - start;
-  var current = start;
-  var increment = end > start? 1 : -1;
-  var obj = document.getElementById(id);
-  var startTime = new Date();
-  var offset = 1;
-  var remainderTime = 0;
-  
-  var step = function() {
-    current += increment;
-    obj.innerHTML = current;
-    
-    if (current != end) {
-      setTimeout(step, easing(duration, range, current));
-    }
-    else {
-      console.log('Easing: ', easing);
-      console.log('Elapsed time: ', new Date() - startTime)
-      console.log('');
-    }
-  };
-  
-  setTimeout(step, easing(duration, range, start));
-}
-
-animateValue("tools-number-animation", 0, 3000, constant);
-
 // ------------------------------------------------------- auto scroll tools menu by click ---------------------------------------------------------------------
 
 $.fn.tabbing = function (options) {
