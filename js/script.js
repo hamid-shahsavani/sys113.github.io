26a5d19293bee97663067c42a3629be8f7bd85d2
sys113 <051.SYS113@gmail.com>
2022-02-23 22:16:44 +0000

    update tools menu
    * add touch scroll support for mobile
    
    * auto scroll with active li

--- js/script.js
+++ js/script.js
@@ -270,3 +270,37 @@ function animateValue(id, start, duration, easing) {
 }
 
 animateValue("tools-number-animation", 0, 3000, constant);
+
+// ------------------------------------------------------- auto scroll tools menu by click ---------------------------------------------------------------------
+
+$.fn.tabbing = function (options) {
+  var opts = {delayTime : 300};
+  options = options || {};
+  opts = $.extend(opts,options);    
+  return this.each(function () {
+      $(this).on('click', function (event) {
+          event.preventDefault();
+          var sum = 0;
+          $(this).prevAll().each(function(){  sum += $(this).width();});
+        var get = document.getElementById('tabs').scrollWidth
+          var dist = sum - ( $(this).parent().width() - $(this).width()) / 2;
+        if(dist < 0){
+          dist = 0;
+        }
+        /* else if(dist+sum > get){
+          dist = get-sum+dist+dist;
+        } */
+          $(this).parent().animate({
+              scrollLeft: dist
+          },opts['delayTime']);
+      });
+  });
+};
+$('#tabs li').tabbing();
+
+
+$('#tabs li').click(function(){
+var  hashit = $(this).find('a').attr('href')
+var autoHeight = $(hashit).height() + 30;
+$('.tab-content').animate({height: autoHeight}, 100);
+});
